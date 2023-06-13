0c0c0a6955b2c14a97802682d30f961f228618c9
Cooper <cmphill@pm.me>
2023-06-13 16:33:13 -0600

    completed function creation

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const drivers = ["Stepehn", "Sarah", "John", "David"]
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0,2)
+};
+
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(-2)
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiplyValue) {
+    return function (value) {
+        return multiplyValue * value
+    }
+}
+  
+    const quad = createFareMultiplier(2)
+    console.log(quad(4))
+
+    const fareDoubler = createFareMultiplier(2)
+    const fareTripler = createFareMultiplier(3)
+    const selectDifferentDrivers = function (drivers, driverToReturn) {
+        return driverToReturn(drivers)
+    }
+    console.log(selectDifferentDrivers(name, returnLastTwoDrivers))
